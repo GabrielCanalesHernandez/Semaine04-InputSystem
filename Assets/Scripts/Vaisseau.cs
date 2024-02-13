4f5a4b5a2d7bf3f146e7eafc806d619664929133
GabrielCanalesHernandez <gabriel.canales.dnz@gmail.com>
2024-02-13 09:57:33 -0500

    faire bouger

---
+++ Assets/Scripts/Vaisseau.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Vaisseau : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float _valeurX;
+    private float _valeurY;
+    private Vector2 _mouvementHorizontal;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Bouge();
+    }
+
+    public void RecevoirInfo(InputAction.CallbackContext context){
+        Debug.Log(context.ReadValue<Vector2>()); //(1,0) (-1,0) (0,0)
+    }
+
+    void Bouge(){
+        
+    }
+}
